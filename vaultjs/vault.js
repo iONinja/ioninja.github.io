e0a74f7548a717c8b91f6eeb5045a7081abc5956
iO Ninja <luke.fisklennon@gmail.com>
2016-12-29 10:06:51 +1100

    Create vault.js

---
+++ vaultjs/vault.js
@@ -0,0 +1,98 @@
+if (!Object.prototype.watch) {
+	Object.defineProperty(Object.prototype, "watch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop, handler) {
+			var
+			  oldval = this[prop]
+			, newval = oldval
+			, getter = function () {
+				return newval;
+			}
+			, setter = function (val) {
+				oldval = newval;
+				return newval = handler.call(this, prop, oldval, val);
+			}
+			;
+			
+			if (delete this[prop]) { // can't watch constants
+				Object.defineProperty(this, prop, {
+					  get: getter
+					, set: setter
+					, enumerable: true
+					, configurable: true
+				});
+			}
+		}
+	});
+}
+
+if (!Object.prototype.unwatch) {
+	Object.defineProperty(Object.prototype, "unwatch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop) {
+			var val = this[prop];
+			delete this[prop];
+			this[prop] = val;
+		}
+	});
+}
+
+var vault = {
+	_load: function() {
+		if (localStorage.vault) {
+			this._variables = JSON.parse(localStorage.vault);
+			this.user.new = false;
+			this.user.returning = true;
+		} else {
+			localStorage.vault = JSON.stringify({});
+		};
+	},
+	_save: function() {
+		localStorage.vault = JSON.stringify(this._variables);
+	},
+	_variables: {
+	},
+	_names: [
+	],
+	user: {
+		new: true,
+		returning: false
+	},
+	sync: function() {
+		for (i=0;i<arguments.length;i++) {
+			n = null;
+			for (j in window) {
+				if (window[j] == arguments[i]) {
+					n = j;
+				};
+			};
+			if (n) {
+				this._names.push(n);
+				if (this._variables[n]) {
+					window[n] = this._variables[n];
+				} else {
+					this._variables[n] = window[n];
+				};
+			};
+			this._save();
+			window.watch(n,function(n,v1,v2) {
+				if (vault._names.indexOf(n) > -1) {
+					vault._variables[n] = v2;
+					vault._save();
+				};
+			});
+		};
+	},
+	wipe: function() {
+		this._variables = {};
+		this._names = [];
+		this.user.new = true;
+		this.user.returning = false;
+		delete localStorage.vault;
+	}
+};
+vault._load();
